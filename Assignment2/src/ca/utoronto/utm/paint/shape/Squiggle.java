f40c555fddd51631c8e11fea5d6f85d96055ce91
Alexander Yeara <Alexander@192.168.2.252>
2017-11-15 00:01:20 -0500

    javadoc fix

--- Assignment2/src/ca/utoronto/utm/paint/shape/Squiggle.java
+++ Assignment2/src/ca/utoronto/utm/paint/shape/Squiggle.java
@@ -2,18 +2,32 @@ package ca.utoronto.utm.paint.shape;
 
 import java.util.List;
 
+/**
+ * @author RealSlimShady
+ *
+ */
 public class Squiggle implements RenderableShape {
 	
 	private List<Point> points;
 	
+	/**
+	 * creates a squiggle object
+	 * @param points List of points.
+	 */
 	public Squiggle(List<Point> points) {
 		this.points = points;
 	}
 	
+	/**
+	 * @return list of points in squiggle
+	 */
 	public List<Point> getPoints() {
 		return this.points;
 	}
 	
+	/** adds points to the list of squiggles.
+	 * @param point point to be added
+	 */
 	public void addPoint(Point point) {
 		this.points.add(point);
 	}
--- Assignment2/src/ca/utoronto/utm/paint/shape/Triangle.java
+++ Assignment2/src/ca/utoronto/utm/paint/shape/Triangle.java
@@ -6,30 +6,58 @@ public class Triangle implements RenderableShape {
 	private Point origin;	
 	
 	
+	/**
+	 * Creates a triangle object 
+	 * 
+	 * @param origin
+	 * @param p1
+	 * @param p2
+	 */
 	public Triangle(Point origin, Point p1, Point p2) {
 		this.origin = origin;
 		this.p1 = p1;
 		this.p2 = p2;
 	}
 	
+	/**
+	 * @return origin of triangle
+	 */
 	public Point getOrigin() {
 		return this.origin;
 	}
 	
+	/**
+	 * sets p1 of triangle
+	 * @param p1
+	 */
 	public void setP1(Point p1) {
 		this.p1 = p1;
 	}
+	/**
+	 * @return p1 of triangle
+	 */
 	public Point getP1() {
 		return this.p1;
 	}
 	
+	/**
+	 * sets the origin of the triangle
+	 * @param origin
+	 */
 	public void setOrigin(Point origin) {
 		this.origin = origin;
 	}
+	/**
+	 * @return p2 of the triangle.
+	 */
 	public Point getP2() {
 		return this.p2;
 	}
 	
+	/**
+	 * sets p2 of the triangle.
+	 * @param p2
+	 */
 	public void setP2(Point p2) {
 		this.p2 = p2;
 	}
